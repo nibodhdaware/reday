c066d93c102a08bbf6316c6deab2db95b6238b77
nibodhdaware <nibod1248@gmail.com>
2024-07-27 12:37:50 +0530

    finalising the UI
    Co-authored-by: yash shahir <yashshahir0111@users.noreply.github.com>
    Co-authored-by: Falcon1104 <Falcon1104@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from flask import Flask, render_template
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 app = Flask(__name__)
 
